683ab0e50f7a97bc9511d5c8268b60a3d0592e49
Richard Whiffen <rich@whiffen.orgw>
2021-05-13 17:18:23 -0400

    Changing to red and to 134 neopixes

--- lightsaber/code.py
+++ lightsaber/code.py
@@ -15,14 +15,14 @@ import adafruit_lis3dh
 # CUSTOMIZE YOUR COLOR HERE:
 # (red, green, blue) -- each 0 (off) to 255 (brightest)
 # COLOR = (255, 0, 0)  # red
-COLOR = (100, 0, 255)  # purple
+COLOR = (255, 0, 0)  # purple
 # COLOR = (0, 100, 255) #cyan
 
 # CUSTOMIZE SENSITIVITY HERE: smaller numbers = more sensitive to motion
 HIT_THRESHOLD = 350 # 250
 SWING_THRESHOLD = 125
 
-NUM_PIXELS = 114
+NUM_PIXELS = 134
 # NUM_PIXELS = 85
 NEOPIXEL_PIN = board.D5
 POWER_PIN = board.D10
