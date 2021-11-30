e2c1d5e0f4f92a71a56755065c79205577299510
rwhiff000 <richard_whiffen@cable.comcast.com>
2021-11-29 19:25:34 -0500

    Adding Clue Bonsai project.

---
+++ clue-bonsai/code.py
@@ -0,0 +1,7 @@
+# 11-29-2021 - make the Adafruit clue and bonsai buckaroo detect water
+# for tracking my christmas tree water level.
+#
+# v0.1 just getting started - will copy paste code from adafruit site to start
+# This will be almost exclusively borrowed code.
+#
+# Rich Whiffen
