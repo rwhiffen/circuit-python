f7fd917dec55dee223fc0913fb3acb9577d7458e
rwhiffen <rich@whiffen.org>
2023-10-15 22:22:12 -0400

    Found the feather M4

--- quadruped-robot/code.py
+++ quadruped-robot/code.py
@@ -4,3 +4,20 @@
 # 
 # it looks like I have the wrong feather board...
 # so this is stalled out
+#
+# 10/14/23 - turns out I have a feather M4 to play with 
+#            game on!
+#
+
+import time
+from adafruit_servokit import ServoKit
+
+# Set channels to the number of servo channels on your kit.
+# 8 for FeatherWing, 16 for Shield/HAT/Bonnet.
+
+legs = ServoKit(channels=8)
+
+kit.servo[0].angle = 45
+time.sleep(1)
+kit.server[0].angle = 30
+time.sleep(1)
