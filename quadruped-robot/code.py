8a92f4b946a1f8bc94538261feade68fe700dac3
rwhiffen <rich@whiffen.org>
2023-10-20 22:09:59 -0400

    starting to map this out

--- quadruped-robot/code.py
+++ quadruped-robot/code.py
@@ -12,12 +12,30 @@
 import time
 from adafruit_servokit import ServoKit
 
+#define the min and max degrees per leg segment
+calf_min=25
+calf_max=75
+thigh_min=45
+thigh_max=135
+
 # Set channels to the number of servo channels on your kit.
 # 8 for FeatherWing, 16 for Shield/HAT/Bonnet.
 
+
+def move_limb(thigh, calf):
+    #pass in the int numbers for the thigh motor and leg motor 
+
+    #thigh is outer loop, calf is inner
+    for degree in range(calf_min, calf_max, 5):
+        legs.servo[thigh].angle=degree
+        time.sleep(1)
+        legs.servo[calf].angle=degree
+
+    return
+
 legs = ServoKit(channels=8)
 
-kit.servo[0].angle = 45
+legs.servo[0].angle = 45
 time.sleep(1)
-kit.server[0].angle = 30
+legs.server[0].angle = 30
 time.sleep(1)
